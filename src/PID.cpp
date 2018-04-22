b761349c21d70961f0b176c6b3feef19b0a4412e
Aftab Engaria <aftabengaria@gmail.com>
2018-04-22 15:06:32 +0530

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -21,14 +21,14 @@ PID::PID() {
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-	Kp = Kp;
-	Ki = Ki;
-	Kd = Kd;
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
 }
 
 void PID::UpdateError(double cte) {
-	d_error = cte - p_error
+	d_error = cte - p_error;
 	p_error = cte;
 	i_error += cte;
 	
